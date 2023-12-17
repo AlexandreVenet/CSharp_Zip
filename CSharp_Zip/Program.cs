cae11755c998941fc55559f6207eb910e59830a7
Alexandre Venet <62010466+AlexandreVenet@users.noreply.github.com>
2023-12-17 19:57:03 +0100

    Initial

---
+++ CSharp_Zip/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.IO.Compression;
+
+namespace CSharp_Zip
+{
+	internal class Program
+	{
+		private static string _cheminDossierSource = @".\DossierTest";
+		private static string _cheminDossierSortie = @".\Extraction";
+		private static string _cheminZip = @".\sortie.zip";
+		private static string _cheminZipNew = @".\nouveau.zip";
+
+		static void Main(string[] args)
+		{
+			Nettoyer();
+			ZipperRepertoire();
+			DezipperRepertoire();
+			ManipulerTxt();
+		}
+
+		private static void Nettoyer()
+		{
+			if (File.Exists(_cheminZip))
+			{
+				File.Delete(_cheminZip);
+			}
+
+			if (File.Exists(_cheminZipNew))
+			{
+				File.Delete(_cheminZipNew);
+			}
+
+			if (Directory.Exists(_cheminDossierSortie))
+			{
+				Directory.Delete(_cheminDossierSortie, true);
+			}
+		}
+
+		private static void ZipperRepertoire()
+		{
+			ZipFile.CreateFromDirectory(_cheminDossierSource, _cheminZip);
+		}
+
+		private static void DezipperRepertoire()
+		{
+			ZipFile.ExtractToDirectory(_cheminZip, _cheminDossierSortie, true); // avec remplacement si existant
+		}
+
+		private static void ManipulerTxt()
+		{
+			string cheminExtraction = Path.Combine(_cheminDossierSortie, "Nouveau.txt");
+
+			// Extraire le fichier txt du .zip
+
+			using (ZipArchive zip = ZipFile.OpenRead(_cheminZip))
+			{
+				ZipArchiveEntry? entry = zip.GetEntry("Fichier.txt");
+				if (entry == null)
+				{
+					Console.WriteLine("Le fichier n'existe pas dans l'archive.");
+					return;
+				}
+				entry.ExtractToFile(cheminExtraction, true);
+			}
+
+			// Ajouter du texte
+
+			using (StreamWriter sw = File.AppendText(cheminExtraction))
+			{
+				sw.WriteLine();
+				sw.WriteLine("-----");
+				sw.WriteLine("Texte supplémentaire");
+				sw.WriteLine("-----");
+			}
+
+			// Créer une archive zip et y mettre le nouveau fichier
+
+			using (FileStream sw = File.Create(_cheminZipNew))
+			{
+				using (ZipArchive zip = new(sw, ZipArchiveMode.Update))
+				{
+					ZipArchiveEntry entry = zip.CreateEntryFromFile(cheminExtraction, "Nouveau Fichier.txt");
+				}
+			}
+
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSharp_Zip
+
+Solution de test avec les fichiers ZIP.
+
+Sources
+- [MS Compression Zip](https://learn.microsoft.com/en-us/dotnet/api/system.io.compression.zipfile)
+- [StackOverflow Ajouter un fichier à une archive zip](https://stackoverflow.com/questions/22339260/how-do-i-add-files-to-an-existing-zip-archive)
+
